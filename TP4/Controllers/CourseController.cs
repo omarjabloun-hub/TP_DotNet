c70b51331021db4f2855fe5b7e33a0efa672c0bd
Omar <omar.jabloun@insat.u-carthage.tn>
2022-12-14 03:44:19 +0100

    add view course

---
+++ TP4/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TP4.Data;
+using TP4.Data.Repository;
+using TP4.Models;
+
+namespace TP4.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            // extract all courses from students
+            var contextoptions = new DbContextOptionsBuilder<UniversityContext>().UseSqlite("DataSource=university.db");
+            
+            StudentRepository srepo = new StudentRepository(new UniversityContext(contextoptions.Options));
+            List<Student> students = (List<Student>)srepo.GetStudents();
+            HashSet<string> courses = new HashSet<string>();
+            foreach(var student in students)
+            {
+                courses.Add(student.course);
+            }
+            return View(courses);
+        }
+    }
+}
---
+++ TP4/Data/Repository/StudentRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP4.Models;
+
+namespace TP4.Data.Repository
+{
+    public class StudentRepository 
+    {
+        internal UniversityContext context;
+        internal DbSet<Student> dbSet;
+
+        public StudentRepository(UniversityContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<Student>();
+        }
+        
+        public IEnumerable<Student> GetStudents()
+        {
+            return dbSet.ToList();
+        }
+
+        public Student GetStudentByID(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void InsertStudent(Student student)
+        {
+            dbSet.Add(student);
+        }
+
+        public void DeleteStudent(int studentID)
+        {
+            Student student = dbSet.Find(studentID);
+            dbSet.Remove(student);
+            
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            dbSet.Attach(student);
+            context.Entry(student).State = EntityState.Modified;
+        }
+        
+    }
+}
